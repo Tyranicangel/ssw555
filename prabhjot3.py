7df6853252ff3e0d11de7fbde90c3a24659e2c06
psingh5712 <psingh11@stevens.edu>
2016-11-05 08:02:17 -0700

    US12 and US14 completed

--- prabhjot3.py
+++ prabhjot3.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import defaultdict
 
 # To check that birth occurs before death of an individual
 def birthBeforeDeath(data):
@@ -17,19 +18,21 @@ def birthBeforeDeath(data):
     return outputErrors
 
 # To get death date if any
-def getDeath(data, personId):
-    if personId is not None and 'DEAT' in data[ 'INDI' ][ personId ]:
-        if 'DATE' in data[ 'INDI' ][ personId ][ 'DEAT' ]:
-            return data[ 'INDI' ][ personId ][ 'DEAT' ][ 'DATE' ][ 'VAL' ]
-    else:
-        return None
+def getDeathBirth(data, personId, fieldName):
+    if personId is not None and fieldName in data[ 'INDI' ][ personId ] and \
+       'DATE' in data[ 'INDI' ][ personId ][ fieldName ]:
+        return data[ 'INDI' ][ personId ][ fieldName ][ 'DATE' ][ 'VAL' ]
+    return None
 
 # To output errors of child birth vs. parent death properly
-def outputChildBirthErrors(data, errorList):
+def outputChildBirthErrors(data, errorList, errType):
     outputErrors = ''
     errorList.sort ( key=lambda x: int ( x[ 0 ].replace ( '@' , "" ).replace ( 'I' , "" ) ) )
     for childId, parentId in errorList:
-        outputErrors +='\nError: US09: birth of a child ' + childId + ' seems to occur after death of mother or before nine months of death of father ' + parentId
+        if errType == 'US09':
+            outputErrors +='\nError: US09: birth of a child ' + childId + ' seems to occur after death of mother or before nine months of death of father ' + parentId
+        elif errType == 'US12':
+            outputErrors +='\nError: US12: Mother should be less than 60 years older than her children and father should be less than 80 years older than his children: ' + childId + ', ' + parentId
     return outputErrors
 
 # To check that child is born before death of mother
@@ -43,12 +46,12 @@ def childBirth(data):
                 momId = entry[ 'WIFE' ][ 'VAL' ]
             else:
                 momId = None
-            momDeathDate = getDeath ( data , momId )
+            momDeathDate = getDeathBirth ( data , momId , 'DEAT' )
             if 'HUSB' in entry:
                 dadId = entry[ 'HUSB' ][ 'VAL' ]
             else:
                 dadId = None
-            dadDeathDate = getDeath ( data , dadId )
+            dadDeathDate = getDeathBirth ( data , dadId , 'DEAT' )
             if len ( entry[ 'CHIL' ] ) == 1:
                 childrenList = [ entry[ 'CHIL' ] ]
             else:
@@ -62,8 +65,8 @@ def childBirth(data):
                     if dadDeathDate is not None and dadDeathDate + datetime.timedelta ( days = 275 ) < childBirthDate:
                         errorDadsList.append ( ( childId, dadId ) )
     outputErrors = ''
-    outputErrors += outputChildBirthErrors ( data, errorMomsList )
-    outputErrors += outputChildBirthErrors ( data, errorDadsList )
+    outputErrors += outputChildBirthErrors ( data, errorMomsList , 'US09' )
+    outputErrors += outputChildBirthErrors ( data, errorDadsList , 'US09' )
     return outputErrors
 
 # To check that marriage occurs before divorce and divorce occurs only after marriage
@@ -159,9 +162,9 @@ def livingRelatives(data):
     outputInfo = ''
     for deceasedId, relIdList in sorted ( list ( relDict.items() ), key=lambda x: int ( x[ 0 ].strip ( '@I' ) ) ):
         if relIdList == [ ]:
-            outputInfo += '\nINFO: US37: A person ' + deceasedId + ', who died in the last 30 days, had no living descendants/spouses'
+            outputInfo += '\nINFO: US37: A person ' + deceasedId + ', who died in the last 30 days, had no living relatives'
         else:
-            outputInfo += '\nINFO: US37: A person ' + deceasedId + ', who died in the last 30 days, had living descendants/spouses ' + ', '.join ( relIdList )
+            outputInfo += '\nINFO: US37: A person ' + deceasedId + ', who died in the last 30 days, had living relatives ' + ', '.join ( relIdList )
     return outputInfo 
 
 # To detect all living couples whose marriage anniversary will occur in the next 30 days 
@@ -185,6 +188,64 @@ def upcomingAnniversaries(data):
         outputInfo += '\nINFO: US39: A living couple ' + id + ' (' + entry[ 'HUSB' ][ 'VAL' ] + ', ' + entry[ 'WIFE' ][ 'VAL' ] + ') discovered whose marriage anniversary is to occur in the next 30 days'
     return outputInfo
 
+def getCheckDate(data, personId, fieldName, delta):
+    birthDate = getDeathBirth ( data , personId , fieldName )
+    if birthDate is not None:
+        return birthDate.replace ( year = birthDate.year + delta )
+    return None
+
+# To check that mother is less than 60 years older than her children and father is less than 80 years older than his children
+def checkParentsAges(data):
+    errorMomsList = [ ]
+    errorDadsList = [ ]
+    for id, entry in data[ 'FAM' ].items():
+        if 'CHIL' in entry:
+            if 'WIFE' in entry:
+                momId = entry[ 'WIFE' ][ 'VAL' ]
+            else:
+                momId = None
+            momCheckDate = getCheckDate( data, momId , 'BIRT', 60)
+
+            if 'HUSB' in entry:
+                dadId = entry[ 'HUSB' ][ 'VAL' ]
+            else:
+                dadId = None
+            dadCheckDate = getCheckDate( data, dadId , 'BIRT', 80)
+
+            childrenList = getMultipleVals ( entry[ 'CHIL' ] )
+
+            for childId in childrenList:
+                if 'BIRT' in data[ 'INDI' ][ childId ]:
+                    childBirthDate = data[ 'INDI' ][ childId ][ 'BIRT' ][ 'DATE' ][ 'VAL' ]
+                    if momCheckDate is not None and momCheckDate < childBirthDate:
+                        errorMomsList.append ( ( childId, momId ) )
+                    if dadCheckDate is not None and dadCheckDate < childBirthDate:
+                        errorDadsList.append ( ( childId, dadId ) )
+    outputErrors = ''
+    outputErrors += outputChildBirthErrors ( data, errorMomsList , 'US12' )
+    outputErrors += outputChildBirthErrors ( data, errorDadsList , 'US12' )
+    return outputErrors
+
+# To check that no more than five siblings are born at the same time
+def checkFiveSiblings(data):
+    errList = [ ]
+    for famId, entry in data[ 'FAM' ].items():
+        if 'CHIL' in entry:
+            childrenList = getMultipleVals ( entry[ 'CHIL' ] )
+            birthDateDict = defaultdict(list)
+            for childId in childrenList:
+                if 'BIRT' in data[ 'INDI' ][ childId ]:
+                    childBirthDate = data[ 'INDI' ][ childId ][ 'BIRT' ][ 'DATE' ][ 'VAL' ].strftime ( '%m/%d/%Y' )
+                    birthDateDict[ childBirthDate ].append ( childId )
+            for date, idList in birthDateDict.items():
+                if len(idList) > 5:
+                    errList.append ( ( famId , sorted ( idList, key=lambda x: int ( x.strip ( '@I' ) ) ) ) )
+    outputErrors = ''
+    for famId, siblingList in errList:
+        outputErrors += '\nError: US14: More than five siblings in family ' + famId + ' were born at the same time: ' + ', '.join ( siblingList )
+    return outputErrors
+                
+
 def run(out):
-    return birthBeforeDeath ( out ) + childBirth ( out ) + marriageDivorce( out ) + lastName ( out ) + livingRelatives ( out ) + upcomingAnniversaries ( out )
+    return birthBeforeDeath ( out ) + childBirth ( out ) + marriageDivorce( out ) + lastName ( out ) + livingRelatives ( out ) + upcomingAnniversaries ( out ) + checkParentsAges ( out ) + checkFiveSiblings ( out )
 
